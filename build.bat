cf0948129cd296fd6a22ecf351033dc7fea4e2d5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-04-12 11:13:20 +0900

    first commit

---
+++ build.bat
@@ -0,0 +1,2 @@
+lualatex --shell-escape math-sample-ja.tex
+lualatex --shell-escape math-sample-ja.tex
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/* 日本語も入れてみる */
+int main(void) {
+    printf("Hello, world\n");
+    return 0;
+}
